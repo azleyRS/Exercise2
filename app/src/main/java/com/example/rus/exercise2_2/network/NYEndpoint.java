4283336be07369b656e4b98fd3c67f395cd32337
Khomyakov Ruslan <azley@mail.ru>
2018-10-31 11:31:12 +0300

    unused calls deleted

--- app/src/main/java/com/example/rus/exercise2_2/network/NYEndpoint.java
+++ app/src/main/java/com/example/rus/exercise2_2/network/NYEndpoint.java
@@ -1,26 +1,12 @@
 package com.example.rus.exercise2_2.network;
 
 import com.example.rus.exercise2_2.network.dto.NYResponce;
-import com.example.rus.exercise2_2.network.dto.Result;
 
 import io.reactivex.Single;
-import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
 
 public interface NYEndpoint {
-    @GET("{section}.json")
-    Call<NYResponce> getNews(@Path("section") String section,
-                             @Query("api-key") String apiKey);
-
-    @GET("/{section}.json")
-    Single<NYResponce> getNewsRx(@Path("section") String section,
-                             @Query("api-key") String apiKey);
-
-    @GET("{section}.json")
-    Call<NYResponce> getNewsWithoutKey(@Path("section") String section);
-
     @GET("{section}.json")
     Single<NYResponce> getNewsRxWithoutKey(@Path("section") String section);
 }
