2722cafcc6824d4ff9448e8acd0028bc26ead0ae
Khomyakov Ruslan <azley@mail.ru>
2018-11-08 15:19:42 +0300

    Added DAO

---
+++ app/src/main/java/com/example/rus/exercise2_2/db/NewsEntity.java
@@ -0,0 +1,86 @@
+package com.example.rus.exercise2_2.db;
+
+import com.example.rus.exercise2_2.model.Category;
+
+import java.util.Date;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class NewsEntity {
+    @PrimaryKey
+    @NonNull
+    private String id;
+    @ColumnInfo
+    private String title;
+    @ColumnInfo
+    private String imageUrl;
+    @ColumnInfo
+    private Category category;
+    @ColumnInfo
+    private Date publishDate;
+    @ColumnInfo
+    private String previewText;
+    @ColumnInfo
+    private String fullText;
+
+    @NonNull
+    public String getId() {
+        return id;
+    }
+
+    public void setId(@NonNull String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public String getPreviewText() {
+        return previewText;
+    }
+
+    public void setPreviewText(String previewText) {
+        this.previewText = previewText;
+    }
+
+    public String getFullText() {
+        return fullText;
+    }
+
+    public void setFullText(String fullText) {
+        this.fullText = fullText;
+    }
+}
---
+++ app/src/main/java/com/example/rus/exercise2_2/utils/FromNetToDbConverter.java
@@ -0,0 +1,11 @@
+package com.example.rus.exercise2_2.utils;
+
+public class FromNetToDbConverter {
+    public void fromDatabase() {
+
+    }
+
+    public void toDatabase() {
+
+    }
+}
