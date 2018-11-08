ede465cb9cfec4c12282b7577cb50e55d5889193
Khomyakov Ruslan <azley@mail.ru>
2018-11-08 14:10:02 +0300

    IntroActivity added, xml created

---
+++ app/src/main/java/com/example/rus/exercise2_2/ui/IntroActivity/IntroActivity.java
@@ -0,0 +1,16 @@
+package com.example.rus.exercise2_2.ui.IntroActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.rus.exercise2_2.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
