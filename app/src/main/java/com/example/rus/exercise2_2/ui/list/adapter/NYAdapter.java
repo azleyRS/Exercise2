96d2126c95b792ad75c15988d008464328837c2c
Khomyakov Ruslan <azley@mail.ru>
2018-11-25 19:25:45 +0300

    pr recommendations no more commented sections

--- app/src/main/java/com/example/rus/exercise2_2/ui/list/adapter/NYAdapter.java
+++ app/src/main/java/com/example/rus/exercise2_2/ui/list/adapter/NYAdapter.java
@@ -14,12 +14,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class NYAdapter extends RecyclerView.Adapter<NYViewHolder> {
 
-    //private List<Result> resultList;
     private List<NewsEntity> newsEntities;
     private final Context context;
 
     public NYAdapter(Context context) {
-        //this.resultList = new ArrayList<>();
         this.newsEntities = new ArrayList<>();
         this.context = context;
     }
@@ -33,21 +31,13 @@ public class NYAdapter extends RecyclerView.Adapter<NYViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull NYViewHolder holder, int position) {
-        //holder.bind(resultList.get(position));
         holder.bind(newsEntities.get(position));
     }
 
     @Override
     public int getItemCount() {
-        //return resultList.size();
         return newsEntities.size();
     }
-
-    /*public void update(List<Result> resultList) {
-        this.resultList = resultList;
-        notifyDataSetChanged();
-    }*/
-
     public void updateWithNewsEntities(List<NewsEntity> newsEntities) {
         this.newsEntities = newsEntities;
         notifyDataSetChanged();
